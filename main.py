f0fc67bfc8369e89a39ae864d4542982c46462df
lordpomind0001 <118119431+lordpomind0001@users.noreply.github.com>
2023-05-06 22:27:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import os
+import shutil
 
-os.system("python Os/Os.py")
+def run():
+    os.system("python Os/Os.py")
+run()
+print("<Os Terminal>")
+def ter():
+    t = input("")
+    if t == "fix basic":
+        print("Fixing os")
+        shutil.rmtree("Os/Data")
+        print("Fixed")
+        ter()
+    elif t == "fix hard":
+        n = input("are you sure that will reinstall everything.")
+        if n == "y":
+            pass
+        if n == "n":
+            print("Cancelled")
+            ter()
+        os.system("git clone ")
+        shutil.copyfile("PROJECT-OS/main.py", "Os/main.py")
+        shutil.rmtree("PROJECT-OS")
+        print("Done")
+        ter()
+    if t == "run":
+        try:
+            run()
+            ter()
+        except:
+            print("failed")
+            ter()
+ter()
